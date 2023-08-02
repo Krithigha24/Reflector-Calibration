117900c82b344709bb9597d99e29a9eab6201ce7
Krithigha <krithigha242000@gmail.com>
2023-08-02 12:08:00 +0800

    test

--- intern_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/krithigha/Reflector-Calibration/intern_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/krithigha/Reflector-Calibration/intern_ws/devel;/opt/ros/noetic'.split(';') if '/home/krithigha/Reflector-Calibration/intern_ws/devel;/opt/ros/noetic' != '' else []
